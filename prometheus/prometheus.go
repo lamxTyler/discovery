8981b7d3d1e7a0d29f4d05ff6ac4697f3bb64be9
Haibin Xie <lambdax.tyler@gmail.com>
2021-10-22 17:59:05 +0800

    update

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -1,155 +1,203 @@
 package prometheus
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lamxTyler/discovery/utils/host"
 	"github.com/prometheus/client_golang/prometheus"
+	io_prometheus_client "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"github.com/ziipin-server/niuhe"
 )
 
 // ** 通用Metrics start **
-// 目前通用部分主要覆盖niuhe自动注册的api
-const (
-	Module = "self"
-)
-
-type Options struct {
-	ServiceName      string
-	Idc              string
-	WatchPath        map[string]struct{}
-	HistogramBuckets []float64
+type promConfig struct {
+	serviceName string
+	host        string
+	pushAddr    string
+	watchPath   map[string]struct{}
+	counter     *prometheus.CounterVec
+	latency     *prometheus.HistogramVec
+	push        *prometheus.CounterVec
+	constLabels []*io_prometheus_client.LabelPair
+	watchAll    bool
+	register    prometheus.Registerer
+	gather      prometheus.Gatherer
 }
 
-type LatencyRecord struct {
-	Time   float64
-	Api    string
-	Module string
-	Method string
-	Code   int
+var defaultConfig *promConfig
+
+func GetMonitorWrapper() *promConfig {
+	return defaultConfig
 }
 
-type QpsRecord struct {
-	Times  float64
-	Api    string
-	Module string
-	Method string
-	Code   int
-}
-
-type Prometheus struct {
-	ServiceName string
-	Idc         string
-	WatchPath   map[string]struct{}
-	Counter     *prometheus.CounterVec
-	Histogram   *prometheus.HistogramVec
-}
-
-var Metrics *Prometheus
-
-func GetMonitorWrapper() *Prometheus {
-	return Metrics
-}
-
-func InitCommonMonitoring(options Options) {
-	if strings.TrimSpace(options.ServiceName) == "" || strings.TrimSpace(options.Idc) == "" || len(options.HistogramBuckets) == 0 {
-		panic(options.ServiceName + " or " + options.Idc + " or HistogramBuckets is empty !!!")
+func InitMonitoring(svr *niuhe.Server, pushIntervalSec int, pushAddr, serviceName string, watchAll bool, watchPaths []string) {
+	if strings.TrimSpace(serviceName) == "" || strings.TrimSpace(pushAddr) == "" {
+		panic(serviceName + " or pushAddr is empty !!!")
+	}
+	buckets := []float64{
+		0.5, 1, 3, 5, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200, 400, 700, 1000, 2000, 3000, 5000, 10000}
+	watchPathSet := make(map[string]struct{})
+	for _, watchPath := range watchPaths {
+		watchPathSet[watchPath] = struct{}{}
 	}
 
-	Metrics = &Prometheus{
-		ServiceName: options.ServiceName,
-		Idc:         options.Idc,
-		WatchPath:   options.WatchPath,
-		Counter: prometheus.NewCounterVec( // QPS and failure ratio
+	defaultConfig = &promConfig{
+		serviceName: serviceName,
+		host:        host.GetHost(),
+		pushAddr:    strings.TrimSpace(pushAddr) + "/api/metrics/add/",
+		watchPath:   watchPathSet,
+		counter: prometheus.NewCounterVec( // QPS and failure ratio
 			prometheus.CounterOpts{
 				Name: "module_responses",
 				Help: "used to calculate qps and failure ratio",
 			},
-			[]string{"app", "module", "api", "method", "code", "idc"},
+			[]string{"api", "method", "code"},
 		),
-		Histogram: prometheus.NewHistogramVec( // P95/P99
+		latency: prometheus.NewHistogramVec( // P95/P99
 			prometheus.HistogramOpts{
 				Name:    "response_duration_milliseconds",
 				Help:    "HTTP latency distributions",
-				Buckets: options.HistogramBuckets,
+				Buckets: buckets,
 			},
-			[]string{"app", "module", "api", "method", "idc"},
+			[]string{"api", "method", "code"},
 		),
+		push: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "prom_metric_push_total",
+				Help: "Total number of pushes.",
+			},
+			[]string{"state"},
+		),
+		watchAll: watchAll,
+		register: prometheus.DefaultRegisterer,
+		gather:   prometheus.DefaultGatherer,
 	}
+	labels := []string{"job", serviceName, "instance", defaultConfig.host}
+	defaultConfig.constLabels = []*io_prometheus_client.LabelPair{
+		{Name: &labels[0], Value: &labels[1]},
+		{Name: &labels[2], Value: &labels[3]},
+	}
+	defaultConfig.register.MustRegister(defaultConfig.counter)
+	defaultConfig.register.MustRegister(defaultConfig.latency)
+	defaultConfig.register.MustRegister(defaultConfig.push)
+	go func() {
+		if pushIntervalSec <= 0 {
+			pushIntervalSec = 60
+		}
+		t := time.NewTicker(time.Duration(pushIntervalSec) * time.Second)
+		defer t.Stop()
+		for {
+			<-t.C
+			defaultConfig.PushMetrics()
+		}
+	}()
 
-	prometheus.MustRegister(Metrics.Counter)
-	prometheus.MustRegister(Metrics.Histogram)
+	svr.Use(prometheusMiddlewareHandler())
 }
 
-// QPS
-func (metrics *Prometheus) QpsCounterLog(record QpsRecord) {
-	if strings.TrimSpace(record.Module) == "" {
-		record.Module = Module
+func NewCollecter(pushAddr, serviceName, host string, registry *prometheus.Registry) *promConfig {
+	config := &promConfig{
+		serviceName: serviceName,
+		host:        host,
+		pushAddr:    strings.TrimSpace(pushAddr) + "/api/metrics/add/",
+		push:        prometheus.NewCounterVec(prometheus.CounterOpts{}, []string{"state"}),
+		register:    registry,
+		gather:      registry,
 	}
-
-	metrics.Counter.WithLabelValues(
-		metrics.ServiceName,
-		record.Module,
-		record.Api,
-		record.Method,
-		strconv.Itoa(record.Code),
-		metrics.Idc,
-	).Add(record.Times)
+	labels := []string{"job", serviceName, "instance", config.host}
+	config.constLabels = []*io_prometheus_client.LabelPair{
+		{Name: &labels[0], Value: &labels[1]},
+		{Name: &labels[2], Value: &labels[3]},
+	}
+	return config
 }
 
-// P95/P99
-func (metrics *Prometheus) LatencyLog(record LatencyRecord) {
-	if strings.TrimSpace(record.Module) == "" {
-		record.Module = Module
-	}
+func (pc *promConfig) LogQuery(api, method string, code int, dur time.Duration) {
+	codeStr := strconv.Itoa(code)
+	pc.counter.WithLabelValues(
+		api,
+		method,
+		codeStr,
+	).Add(1)
 
-	metrics.Histogram.WithLabelValues(
-		metrics.ServiceName,
-		record.Module,
-		record.Api,
-		record.Method,
-		metrics.Idc,
-	).Observe(record.Time)
+	mSec := dur / time.Millisecond
+	nSec := dur % time.Millisecond
+	latency := float64(mSec) + float64(nSec)/1e6
+	pc.latency.WithLabelValues(
+		api,
+		method,
+		codeStr,
+	).Observe(latency)
 }
 
-func PrometheusMiddlewareHandler() gin.HandlerFunc {
+func (pc *promConfig) PushMetrics() {
+	mfs, err := pc.gather.Gather()
+	if err != nil {
+		pc.push.WithLabelValues("gather-error").Inc()
+		niuhe.LogError("gather metrics failed: %s", err.Error())
+		return
+	}
+	buf := &bytes.Buffer{}
+	enc := expfmt.NewEncoder(buf, expfmt.FmtText)
+	now := time.Now()
+	timeStamp := now.Unix()*1000 + int64(now.Nanosecond()/1000000)
+	for _, mf := range mfs {
+		for _, m := range mf.Metric {
+			m.Label = append(m.Label, pc.constLabels...)
+			m.TimestampMs = &timeStamp
+		}
+		err := enc.Encode(mf)
+		if err != nil {
+			pc.push.WithLabelValues("encode-error").Inc()
+			niuhe.LogError("encode metrics failed: %s", err.Error())
+			return
+		}
+	}
+
+	values := map[string]string{
+		"service_name":  pc.serviceName,
+		"instance":      pc.host,
+		"metrics_datas": buf.String(),
+	}
+	data, err := json.Marshal(values)
+	if err != nil {
+		pc.push.WithLabelValues("marshal-error").Inc()
+		niuhe.LogError("marshal json failed: %s", err.Error())
+		return
+	}
+	resp, err := http.Post(pc.pushAddr, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		pc.push.WithLabelValues("push-error").Inc()
+		niuhe.LogError("push metrics failed: %s", err.Error())
+		return
+	}
+	resp.Body.Close()
+	pc.push.WithLabelValues("success").Inc()
+}
+
+func prometheusMiddlewareHandler() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		start := time.Now()
 		context.Next()
 
-		go func() {
-			if GetMonitorWrapper() != nil {
-				if _, ok := Metrics.WatchPath[context.Request.URL.Path]; ok {
-					// QPS
-					Metrics.QpsCounterLog(QpsRecord{
-						Times:  1,
-						Api:    context.Request.URL.Path,
-						Module: Module,
-						Method: context.Request.Method,
-						Code:   context.Writer.Status(),
-					})
-
-					// P95/P99
-					d := time.Since(start)
-					mSec := d / time.Millisecond
-					nSec := d % time.Millisecond
-					latency := float64(mSec) + float64(nSec)/1e6
-					Metrics.LatencyLog(LatencyRecord{
-						Time:   latency,
-						Api:    context.Request.URL.Path,
-						Module: Module,
-						Method: context.Request.Method,
-						Code:   context.Writer.Status(),
-					})
-
-					//niuhe.LogInfo("access logger => method %v, path %v, status %v, latency %v", context.Request.Method, context.Request.URL.Path, context.Writer.Status(), latency)
-				}
-			} else {
-				niuhe.LogWarn("need to init prometheus!!!")
+		status := context.Writer.Status()
+		if defaultConfig == nil || status == 404 {
+			return
+		}
+		if !defaultConfig.watchAll {
+			if _, ok := defaultConfig.watchPath[context.Request.URL.Path]; !ok {
+				return
 			}
-		}()
+		}
+		d := time.Since(start)
+		defaultConfig.LogQuery(context.Request.URL.Path, context.Request.Method, status, d)
 	}
 }
 
@@ -158,22 +206,6 @@ func PrometheusMiddlewareHandler() gin.HandlerFunc {
 // ** 自定义模块(不在niuhe里注册的那部分api)使用示例 start **
 // 延迟
 // latencyMS := endTime - startTime // your_api_lantency
-// prometheus.GetMonitorWrapper().LatencyLog(prometheus.LatencyRecord{
-//  Api:  "/api/account/inner_check_token/",
-// 	Module: "uc",
-// 	Method: "POST",
-//  me: latencyMS
-// })
-
-// QPS
-// prometheus.GetMonitorWrapper().QpsCountLog(prometheus.QPSRecord{
-//    Module: "uc",
-//    Times:  float64(1),
-//    Api:    "/api/account/inner_check_token/",
-//    Method: "POST",
-//    Code:   200, // your api's code
-// })
-
+// prometheus.GetMonitorWrapper().LogQuery("/api/account/inner_check_token/", "POST", 200, latencyMS)
 // ** 自定义模块 end **
-
 // **如有更多的需求，可以扩展
---
+++ utils/host/host.go
@@ -0,0 +1,41 @@
+package host
+
+import (
+	"net"
+	"os"
+	"strings"
+
+	"github.com/ziipin-server/niuhe"
+)
+
+func GetHost() string {
+	os.Hostname()
+	host := os.Getenv("HOSTNAME")
+	if host != "" {
+		return host
+	}
+	return GetInnerIp()
+}
+
+func GetInnerIp() string {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		niuhe.LogError("net.Interfaces failed, err:%v", err)
+		return ""
+	}
+
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil && strings.HasPrefix(ipnet.IP.String(), "10.") {
+						return ipnet.IP.String()
+					}
+				}
+			}
+		}
+	}
+	return ""
+}
